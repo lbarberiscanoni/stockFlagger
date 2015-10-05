392866fffd7a197097b6368d5d511986a8283ab9
lbarberiscanoni <hllbck7@gmail.com>
2015-10-05 09:29:55 -0700

    changed and started using the googlefinance api bc it's faster

--- flagger.py
+++ flagger.py
@@ -1,5 +1,5 @@
 import subprocess
-from yahoo_finance import *
+from googlefinance import *
 from datetime import *
 
 stocksToFollow = {"FNMA": 2.31, "FB": 92, "AAPL": 110}
@@ -8,10 +8,10 @@ while True:
     print timestamp
     for stock in stocksToFollow:
         priceTarget = stocksToFollow[stock]
-        priceAtThisMoment = Share(stock).get_price()
+        priceAtThisMoment = getQuotes(stock)[0]["LastTradePrice"]
         print stock + ": " + priceAtThisMoment 
 
         if priceAtThisMoment == priceTarget:
-            stockUpdate = "python gmailText.py -u hllbck7 -p l0ll02012 -t 8642436724@text.att.net -b '" + stock + " is at " + priceAtThisMoment + "'"
+            stockUpdate = "python gmailText.py -u hllbck7@gmail.com -p l0ll02012 -t 8642436724@text.att.net -b '" + stock + " is at " + priceAtThisMoment + "'"
             subprocess.call(stockUpdate, shell=True)
     print "-------------------------------"
